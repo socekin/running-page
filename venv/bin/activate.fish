219308c84b70317095d8627842aca07134044c47
socekin <socekin@gmail.com>
2025-05-01 22:21:01 +0800

    chore: update running page

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/jay/running/running_page/venv
+set -gx VIRTUAL_ENV /Users/jay/running-page/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
