2d7e33fb782ea9fcbeab177f494585bd3059f3b1
socekin <socekin@gmail.com>
2025-05-02 21:25:13 +0800

    chore: update running page

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -17,7 +17,7 @@ const getBasePath = () => {
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Jay',
   siteUrl: 'https://run.jaylife.cc',
-  logo: 'https://telegraph-image-7y6.pages.dev/file/86131c954ce23302178d6.png',
+  logo: 'https://s2.loli.net/2025/05/02/3cfszSZrjiWqQ8E.png',
   description: 'Personal site and blog',
   navLinks: [
     {
